6bfeea87e77deed6140dfed4da4184a90af4fe54
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2020-06-04 22:33:09 +0100

    Removed redundant __init__.py files and enabled mypy

--- wx_icons_suru/__init__.py
+++ wx_icons_suru/__init__.py
@@ -23,11 +23,11 @@
 #
 
 # 3rd party
-import importlib_resources
+import importlib_resources  # type: ignore
+from wx_icons_humanity import HumanityIconTheme, wxHumanityIconTheme  # type: ignore  # TODO
 
 # this package
 from wx_icons_suru import Suru
-from wx_icons_humanity import HumanityIconTheme, wxHumanityIconTheme
 
 with importlib_resources.path(Suru, "index.theme") as theme_index_path:
 	theme_index_path = str(theme_index_path)
@@ -36,9 +36,9 @@ __version__ = "0.1.1"
 
 
 def version():
-	return f"""wx_icons_hicolor
+	return f"""wx_icons_suru
 Version {__version__}
-Gnome Icon Theme Version 20.04.4
+Suru Icon Theme Version 20.04.4
 """
 
 
@@ -102,6 +102,6 @@ if __name__ == '__main__':
 	# for directory in theme.directories:
 	# 	print(directory.icons)
 
-	from wx_icons_hicolor import test_random_icons, test
+	from wx_icons_hicolor import test_random_icons, test  # type: ignore  # TODO
 	# test_random_icons(theme)
 	test.test_icon_theme(theme, show_success=False)
