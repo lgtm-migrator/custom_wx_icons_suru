1a5a024343f8ec456727cfd091f90ce8bd971e92
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2020-11-25 09:13:06 +0000

    Removed unused # type: ignore comment.

--- wx_icons_suru/__init__.py
+++ wx_icons_suru/__init__.py
@@ -26,7 +26,7 @@
 from typing import Any, Optional, Tuple, Union
 
 # 3rd party
-import importlib_resources  # type: ignore
+import importlib_resources
 import wx  # type: ignore
 from wx_icons_hicolor import Icon, IconTheme
 from wx_icons_humanity import HumanityIconTheme, wxHumanityIconTheme
